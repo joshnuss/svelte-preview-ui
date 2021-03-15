7806176b20176fccec4d70e6c91c7cef749c7138
Josh Nussbaum <joshnuss@gmail.com>
2021-03-14 21:50:30 -0400

    Adds BrowserScreen component

---
+++ src/BrowserScreen.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Screen from './Screen.svelte'
+
+  export let address = "https://google.com"
+</script>
+
+<Screen>
+  <div class="address-bar" slot="bar">
+    <input value={address}/>
+  </div>
+
+  <slot/>
+</Screen>
+
+<style>
+  .address-bar {
+    display: flex;
+    padding: 3px;
+    margin: 5px 5px 5px 100px;
+  }
+  .address-bar input {
+    background: var(--screen-shadow-color);
+    border: none;
+    padding: 0.2rem 1rem;
+    border-radius: 15px;
+    font-size: 1.2rem;
+    display: block;
+    width: 100%;
+  }
+</style>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as Screen } from './Screen.svelte'
+export { default as BrowserScreen } from './BrowserScreen.svelte'
 export { default as Phone } from './Phone.svelte'
